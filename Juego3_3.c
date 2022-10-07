ea9a34714f6c441103fa713d1da48e2d35efc470
tahaskaf <115151015+tahaskaf@users.noreply.github.com>
2022-10-07 19:18:48 +0200

    Update Juego3_3.c

--- Juego3_3.c
+++ Juego3_3.c
@@ -6,13 +6,11 @@ int main(int argc, char **argv)
 {
 	MYSQL *conn;
 	int err;
-	char nombre[25];
-	char pass[15];
-	char id[10];
-	int i;
-	char consulta [80];
+	// Estructura especial para almacenar resultados de consultas 
+	MYSQL_RES *resultado;
+	MYSQL_ROW row;
 	
-		
+	//Creamos una conexion al servidor MYSQL 
 	conn = mysql_init(NULL);
 	if (conn==NULL) {
 		printf ("Error al crear la conexion: %u %s\n", 
@@ -26,33 +24,33 @@ int main(int argc, char **argv)
 				mysql_errno(conn), mysql_error(conn));
 		exit (1);
 	}
-	for (i=0; i<4; i++)
-		printf ("Escribe el nombre, contrasenya y id de la persona, separados por un blanco\n") ;
-	err = scanf ("%s %s %s", nombre, pass, id);
-	if (err!=3)
-	{
-		printf ("Error al introducir los datos \n");
-			exit (1);
-	}
 	
-	strcpy (consulta, "INSERT INTO JUGADOR VALUES('");
-	strcat (consulta, nombre);
-	strcat (consulta, "','");
+	char consulta [80];
+	strcpy (consulta,"SELECT COUNT(DISTINCT JUGADOR.id) FROM JUGADOR,PARTIDA,PARTICIPACION WHERE PARTIDA.duracion > 60 AND JUGADOR.id = PARTICIPACION.JUGADOR AND PARTICIPACION.PARTIDA = PARTIDA.id");
 	
-	strcat (consulta, pass);
-	strcat (consulta, "','");
-	
-	strcat (consulta, id);
-	strcat (consulta, "','");
-	
-	printf("consulta = %s\n", consulta);
-	err = mysql_query(conn, consulta);
-	
+	err=mysql_query (conn, consulta);
 	if (err!=0) {
 		printf ("Error al consultar datos de la base %u %s\n",
 				mysql_errno(conn), mysql_error(conn));
 		exit (1);
 	}
+	//recogemos el resultado de la consulta. El resultado de la
+	//consulta se devuelve en una variable del tipo puntero a
+	//MYSQL_RES tal y como hemos declarado anteriormente.
+	//Se trata de una tabla virtual en memoria que es la copia
+	//de la tabla real en disco.
+	resultado = mysql_store_result (conn);
+	// El resultado es una estructura matricial en memoria
+	// en la que cada fila contiene los datos de una persona.
+	
+	// Ahora obtenemos la primera fila que se almacena en una
+	// variable de tipo MYSQL_ROW
+	row = mysql_fetch_row (resultado);
+	
+	if (row == NULL)
+		printf ("No se han obtenido datos en la consulta\n");
+	else
+		printf ("%s\n", row[0]);
 	
 	mysql_close (conn);
 	exit(0);
